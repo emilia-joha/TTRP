4cb32d287128c29049506a655d3c98f5e33e2fa1
Emilia <56506910+emilia-joha@users.noreply.github.com>
2023-07-26 22:25:49 +0200

    Update abilities.js

--- scripts/abilities.js
+++ scripts/abilities.js
@@ -5,8 +5,9 @@ const skillHTML = skills.map(function (skill) {
   return `
     <div class="skill">
         <label>
-            <input id="${skill[3]}" type="checkbox"/>
-            <input id="${skill[2]}" type="checkbox"/>
+            <input id="${skill[3]}" class="expertise" type="checkbox"/>
+            <input id="${skill[2]}" class="proficiency" type="checkbox"/>
+            <p id="${skill[0]}" class="${skill[1]}"> </p>
             ${skill[0]} (${skill[1]})
         </label>
     </div>`;
@@ -19,36 +20,42 @@ $("#ability_strength").keyup(function () {
   const abilityStrength = $(this).val();
   const modifier = abilityCalc(abilityStrength);
   $("#ability_modifier_strength").text(modifier);
+  $(".Str").text(modifier);
 });
 
 $("#ability_dexterity").keyup(function () {
   const ability = $(this).val();
   const modifier = abilityCalc(ability);
   $("#ability_modifier_dexterity").text(modifier);
+  $(".Dex").text(modifier);
 });
 
 $("#ability_constitution").keyup(function () {
   const ability = $(this).val();
   const modifier = abilityCalc(ability);
   $("#ability_modifier_constitution").text(modifier);
+  $(".Con").text(modifier);
 });
 
 $("#ability_intelligence").keyup(function () {
   const ability = $(this).val();
   const modifier = abilityCalc(ability);
   $("#ability_modifier_intelligence").text(modifier);
+  $(".Int").text(modifier);
 });
 
 $("#ability_wisdom").keyup(function () {
   const ability = $(this).val();
   const modifier = abilityCalc(ability);
   $("#ability_modifier_wisdom").text(modifier);
+  $(".Wis").text(modifier);
 });
 
 $("#ability_charisma").keyup(function () {
   const ability = $(this).val();
   const modifier = abilityCalc(ability);
   $("#ability_modifier_charisma").text(modifier);
+  $(".Cha").text(modifier);
 });
 
 $("#level").on("change", function () {
@@ -59,7 +66,7 @@ $("#level").on("change", function () {
 
 let previousSelectedBackground = null;
 
-$("#background").on("change", function (e) {
+$("#background").on("change", function () {
   const selectedBackground = $(this).val();
 
   const background = backgrounds.find((b) => b.name == selectedBackground);
@@ -85,7 +92,6 @@ $("#background").on("change", function (e) {
   background.proficiencies.forEach(function (prof) {
     if (typeof prof == "string") {
       const profLower = prof.toLowerCase().replaceAll(" ", "_");
-      console.log(profLower);
       $(`#proficiency_skill_${profLower}`).prop("checked", true);
     } else {
       alert(
@@ -96,6 +102,21 @@ $("#background").on("change", function (e) {
 
   // kom ihåg vilken som var vald ifall man byter
   previousSelectedBackground = background.name;
+});
+
+$(".proficiency").change(function () {
+  let skillProf = 0;
+  const profBonus = $("#proficiency_bonus").text();
+  const skill = $(this).siblings("p").text();
+  $(this).siblings("p").empty();
+  if (this.checked) {
+    skillProf = Number(skill) + Number(profBonus);
+    console.log(skillProf);
+    $(this).siblings("p").text(skillProf);
+  } else {
+    skillProf = Number(skill) - Number(profBonus);
+    $(this).siblings("p").text(skillProf);
+  }
 });
 
 function abilityCalc(ability) {
